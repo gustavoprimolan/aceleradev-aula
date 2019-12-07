ef57afb6536ed3b0905553baab5e027d74d35496
Gustavo Primolan <gustavo.primolan@hotmail.com>
2019-12-07 09:37:35 -0300

    Atualizando

--- modulo07/biblioteca-aula07-jwt-endpoint-auth/src/main/java/br/com/codenation/biblioteca/utils/PasswordEncoder.java
+++ modulo07/biblioteca-aula07-jwt-endpoint-auth/src/main/java/br/com/codenation/biblioteca/utils/PasswordEncoder.java
@@ -8,6 +8,5 @@ public class PasswordEncoder {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         System.out.println(passwordEncoder.encode("aceleradev"));
     }
-
-
+    
 }
